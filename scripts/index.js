e7d5673be1e43252a39fb78816d9625c5ae9ced7
Prathana <pandeyprathana@gmail.com>
2025-07-10 23:26:01 -0400

    Project 4 stage 3 1st commit

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,24 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileEditBtn = document.querySelector(".profile__edit-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileDesc = document.querySelector(".profile__description");
+const profileEditModal = document.querySelector(".modal");
+const profileCloseBtn = document.querySelector(".modal__close");
+const profileSaveBtn = document.querySelector(".modal__button");
+
+profileEditBtn.addEventListener("click", function () {
+  profileEditModal.classList.add("modal__opened");
+  document.querySelector("#name").value = profileTitle.textContent;
+  document.querySelector("#description").value = profileDesc.textContent;
+});
+
+profileCloseBtn.addEventListener("click", function () {
+  profileEditModal.classList.remove("modal__opened");
+});
+
+profileSaveBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+});
